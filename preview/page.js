6a8013d5a4a713f3aaa78f3c91708c1c4cc26d79
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-02-26 18:57:22 -0800

    Saved at: 2013-02-27 02:57:21 +0000

--- preview/page.js
+++ preview/page.js
@@ -1,17 +1,19 @@
+var currentID = 0;
+
 $(function () {
   
-  var currentID = 0;
+  
   var content = JSON.parse(BL.getContentForPreview().content[0].data);
   $('#text').append('' + content.body + '');
   $('#text').append(' ' + currentID);
   
-  if (currentID == 0){
-    
-  }
-  
  
   document.getElementById('imgContent1').style.display='none';
   document.getElementById('imgContent2').style.display='none';
+  
+  document.getElementById('next').style.display='none';
+  document.getElementById('next2').style.display='none';
+  document.getElementById('next3').style.display='none';
   //document.getElementById('imgContent3').style.display='none';
  
   BL.previewReady();
@@ -19,20 +21,43 @@ $(function () {
 
 function found() {
   document.getElementById('imgContent').style.display='none';
-  document.getElementById('imgContent1').style.display='inline';
+  document.getElementById('next').style.display='inline';
 }
 
 function found2() {
   document.getElementById('imgContent1').style.display='none';
-  document.getElementById('imgContent2').style.display='inline';
+  document.getElementById('next2').style.display='inline';
 }
 
 function found3() {
   document.getElementById('imgContent2').style.display='none';
-  document.getElementById('imgContent3').style.display='inline';
+  document.getElementById('next3').style.display='inline';
 }
 
 function next() {
   document.getElementById('imgContent2').style.display='none';
   document.getElementById('imgContent3').style.display='inline';
 }
+
+function check(){
+  if (currentID==2){
+     currentID = 3;
+    document.getElementById('next3').style.display='none';
+    document.getElementById('imgContent3').style.display='inline';
+  }
+ 
+ if (currentID==1){
+     currentID = 2;
+    document.getElementById('next2').style.display='none';
+    document.getElementById('imgContent2').style.display='inline';
+  }
+  
+  if (currentID==0){
+    currentID = 1;
+    document.getElementById('next').style.display='none';
+    document.getElementById('imgContent1').style.display='inline';
+  }
+  
+ 
+}
+
