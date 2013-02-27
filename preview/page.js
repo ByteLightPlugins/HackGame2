f6fa5fe0b197aff8566fa4500d2634deea496eda
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-02-26 18:33:51 -0800

    Saved at: 2013-02-27 02:33:50 +0000

---
+++ preview/page.js
@@ -0,0 +1,33 @@
+$(function () {
+  
+  var currentID = 0;
+  var content = JSON.parse(BL.getContentForPreview().content[0].data);
+  $('#text').append('' + content.body + '');
+  $('#text').append(' ' + currentID);
+  
+  if (currentID == 0){
+    
+  }
+  
+ 
+  document.getElementById('imgContent1').style.display='none';
+  document.getElementById('imgContent2').style.display='none';
+  //document.getElementById('imgContent3').style.display='none';
+ 
+  BL.previewReady();
+});
+
+function found() {
+  document.getElementById('imgContent').style.display='none';
+  document.getElementById('imgContent1').style.display='inline';
+}
+
+function found2() {
+  document.getElementById('imgContent1').style.display='none';
+  document.getElementById('imgContent2').style.display='inline';
+}
+
+function found3() {
+  document.getElementById('imgContent2').style.display='none';
+  document.getElementById('imgContent3').style.display='inline';
+}
